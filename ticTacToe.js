3b84f5f4909a3d4e5137f0a848ff68d4d108d278
Andy <andrewhardesty8@gmail.com>
2022-01-25 22:58:08 -0800

    added comment, added logic for detecting winner, added logic to toggle turns

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,5 +1,16 @@
+
+// initialize X as the starting player
+let turn = 'O';
+
+// intialize gameBoard array
 let gameBoard = [];
+
+// get the gameBoard node in the DOM
 const board = document.querySelector('.board');
+
+// in the gameBoard array, create 3 rows of 3 cells each
+// on the gameBoard node in the DOM, create divs for each row and for each cell in each row
+// add a clickevent to each DOM cell that associates it with the index in the array
 for (let i = 0; i < 3; i++){
     gameBoard.push([]);
     let row = document.createElement('div');
@@ -10,9 +21,52 @@ for (let i = 0; i < 3; i++){
         let cell = document.createElement('div');
         cell.classList.add('cell');
         cell.addEventListener('click', () => {
-            gameBoard[i][j] = 'X';
-            cell.textContent = gameBoard[i][j];
+            if (gameBoard[i][j] === ''){
+                // set the marker based on which player's turn it is
+                playerTurn();
+                // place player marker in the array and on the board
+                //gameBoard[i][j] = 'X';
+                gameBoard[i][j] = turn;
+                cell.textContent = gameBoard[i][j];
+                // check each adjacent cell for player marker
+                checkWin(i, j);
+            } else {
+                return;
+            }
         })
         row.appendChild(cell);
+    }
+}
+
+// function for checking if a player has won
+function checkWin(i, j){
+    // check current row and column
+    if (gameBoard[i][0]==="X" && gameBoard[i][1]==="X" && gameBoard[i][2]==="X"){
+        alert('You Win!');
+    }
+    if (gameBoard[0][j]==="X" && gameBoard[1][j]==="X" && gameBoard[2][j]==="X"){
+        alert('You Win!')
+    }
+    // check diagonals
+    if ((i === 0 && j === 0) || (i === 1 && j === 1) || (i === 2 && j === 2)){
+        if (gameBoard[0][0] === "X" && gameBoard[1][1] === "X" && gameBoard[2][2] === "X"){
+            alert('You Win!')
+        }
+    }
+    if ((i === 2 && j === 0) || (i === 1 && j === 1) || (i === 0 && j === 2)){
+        if (gameBoard[2][0] === "X" && gameBoard[1][1] === "X" && gameBoard[0][2] === "X"){
+            alert('You Win!')
+        }
+    }
+}
+
+// rotate turns between X and O every time a cell is clicked
+function playerTurn(){
+    if (turn === 'X'){
+        turn = "O";
+        return turn;
+    } else if (turn === 'O'){
+        turn = "X";
+        return turn;
     }
 }
