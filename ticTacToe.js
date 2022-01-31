32c651dbc7e8a6337ee213092d69d1f84249e8dc
Andy <andrewhardesty8@gmail.com>
2022-01-30 16:43:03 -0800

    cleaned up comments

--- ticTacToe.js
+++ ticTacToe.js
@@ -5,64 +5,55 @@ const playerFactory = (name, marker) => {
 }
 
 
-// module Game Board
-// this module will control the game board array and update it when DOM elements are clicked
-// this module will rely on input from the Game Controller module for when the game is over and whose turn it is
-// this module will output the array, which will be used by both the Display Controller and Game Controller modules
-    // Display Controller will update the display to match the array
-    // Game Controller will use the array to check if the game is over and if the current player needs to switch
+// module - Game Board
 const gameBoard = (() => {
-    // initialize the array
+
+    // initialize the gameBoard array
     const boardArray = ['','','','','','','','',''];
-    // get input from Game Controller for if reset button is clicked
-        // if reset button is clicked, re-initialize the array
-        //if (gameController.reset() === 'true'){ ///////////////////////////////////////////////////////////
-        //    boardArray = ['','','','','','','','',''];
-        //} /////////////////////////////////////////////////////////////////////////////////////////////////
-    // update the array when the DOM is clicked
-        // cache the DOM
-        let board = document.querySelector('.board');
-        // add event listener on DOM
-        board.addEventListener('click', _updateArray);
-        // add function for updating array
-        function _updateArray(e){
-            // get game status (over or not) from Game Controller
-            // check if array position is already taken
-            if (gameController.gameStatus() !== 'over' && boardArray[e.target.id] === "") {
-                // get the current player from the Game Controller, and fill the array index
-                boardArray[e.target.id] = gameController.currentPlayer().marker;
-                // pass boardArray to Display Controller to update the display
-                displayController.updateDisplay();
-            } else {
-                return;
-            }
+
+    // reset button //////////////////////////////////////////////////////////
+
+    // cache the DOM
+    let board = document.querySelector('.board');
+
+    // bind events
+    board.addEventListener('click', _updateArray);
+
+    // if game isn't over and target array index is empty, fill index with current player, switch players, and update display
+    function _updateArray(e){
+        if (gameController.gameStatus() !== 'over' && boardArray[e.target.id] === "") {
+            boardArray[e.target.id] = gameController.currentPlayer().marker;
+            gameController.switchPlayer();
+            displayController.updateDisplay();
+        } else {
+            return;
         }
-    // store board array in a function for public use
+    }
+
+    // store boardArray in a function for public use
     function publicArray(){
         return boardArray;
     }
-    
+
     return { publicArray };
 })()
 
 
-// module Display Controller
+// module - Display Controller
 const displayController = (() => {
 
-    // update the display to match the array
-        // cache the DOM
-        let cells = [];
+    // cache the DOM
+    let cells = [];
+    for (let i = 0; i < gameBoard.publicArray().length; i++){
+        cells[i] = document.getElementById(`${i}`)
+    }
+
+    // update the display to match the Game Board array each time the array is updated
+    function updateDisplay(){
         for (let i = 0; i < gameBoard.publicArray().length; i++){
-            cells[i] = document.getElementById(`${i}`)
+            cells[i].textContent = `${gameBoard.publicArray()[i]}`;
         }
-
-        // update the display to match the Game Board array if the array is updated
-        function updateDisplay(){
-            for (let i = 0; i < gameBoard.publicArray().length; i++){
-                cells[i].textContent = `${gameBoard.publicArray()[i]}`;
-            }
-        }
-
+    }
 
     return { updateDisplay }
 
@@ -70,112 +61,44 @@ const displayController = (() => {
     
 
 
-// module Game Controller
+// module - Game Controller
 const gameController = (() => {
 
     // initialize players
     let playerX = playerFactory('Player X', 'X');
     let playerO = playerFactory('Player O', 'O');
+
     // initialize the current player as X
     let player = playerX;
-    // switch players each time the array is updated
-        // initialize controller array to match gameboard array
-        let controllerArray = ['','','','','','','','',''];
-        // switch player if the controllerArray doesn't match the gameBoard array, and update controllerArray
-        // when currentPlayer is executed, the result's marker will go into the target index of boardArray in Game Board
-        function currentPlayer(){
-            for (let i = 0; i < controllerArray.length; i++){
-                if (controllerArray[i] !== gameBoard.publicArray()[i]){
-                    if (player === playerX){
-                        player = playerO;
-                        controllerArray[i] = gameBoard.publicArray()[i];
-                    } else if (player === playerO){
-                        player = playerX
-                        controllerArray[i] = gameBoard.publicArray()[i];
-                    }
-                }
-            }
-            return player;
+
+    // output the current player
+    function currentPlayer(){
+        return player;
+    }
+
+    // switch players each time the currentPlayer function is called
+    function switchPlayer(){
+        if (player === playerX){
+            player = playerO;
+        } else if (player === playerO){
+            player = playerX
         }
+        return player;
+    }
+
+
     // check to see if the current player won each time the array is updated
-        // pass game status to the Game Board, to stop cells from being clicked after game is over
-        function gameStatus() { /////////////////////////////////////////////////////////////////////////
-            return 'notOver';
-        } ///////////////////////////////////////////////////////////////////////////////////////////////
-        // if player wins, tell Display Controller to display the winner
+
+    // pass game status to the Game Board, to stop cells from being clicked after game is over
+    function gameStatus() { /////////////////////////////////////////////////////////////////////////
+        return 'notOver';
+    } ///////////////////////////////////////////////////////////////////////////////////////////////
+
+    // if player wins, tell Display Controller to display the winner
+
     // if reset button is clicked, reset the game and tell Game Board to re-initialize
 
 
-    return { currentPlayer, gameStatus }
+    return { currentPlayer, switchPlayer, gameStatus }
+
 })()
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*const playerOne = playerFactory('Player One', 'X');
-const playerTwo = playerFactory('Player Two', 'O');*/
-
-
-/*const gameBoard = (() => {
-
-    let player = '';
-    let markerArray = ['','','','','','','','',''];
-
-    // cache DOM
-    let board = document.querySelector('.board');
-
-    // bind events
-    board.addEventListener('click', setMarker);
-
-    function setMarker(e){
-        if (markerArray[e.target.id] === "") {
-            switchPlayer();
-            markerArray[e.target.id] = player.marker;
-            e.target.innerText = player.marker;
-        } else {
-            return;
-        }
-    }
-
-    function switchPlayer(){
-        if (player === ''){
-            player = playerOne;
-            return player;
-        }
-        if (player === playerOne){
-            player = playerTwo;
-            return player;
-        } else if (player === playerTwo){
-            player = playerOne;
-            return player;
-        }
-    }
-
-    function publicArray(){
-        return markerArray;
-    }
-
-    return { publicArray }
-})()
-*/
